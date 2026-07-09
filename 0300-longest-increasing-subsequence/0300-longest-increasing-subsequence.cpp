04c825158eabc5a2c9b0d3ac6eeff49e221ad2ec
Subham harbola <146356080+itzmexiao@users.noreply.github.com>
2026-07-09 15:22:27 +0530

    Time: 0 ms (100%), Space: 14.1 MB (77.75%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> tail;
+        for(int num : nums){
+            auto it = lower_bound(tail.begin(), tail.end(), num);
+
+            if(it == tail.end())
+                tail.push_back(num);
+            else
+                *it = num;
+        }
+        return tail.size();
+    }
+};
