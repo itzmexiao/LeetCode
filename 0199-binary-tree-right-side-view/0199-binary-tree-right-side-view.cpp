823f32aee9312e2a5aea34c49a8d1ac5a3cb6a39
Subham harbola <146356080+itzmexiao@users.noreply.github.com>
2026-07-20 14:34:57 +0530

    Time: 0 ms (100%), Space: 15 MB (63.96%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -18,14 +18,10 @@ public:
         q.push(root);
         while (!q.empty()) {
             int size = q.size();
-            // process one level
             for (int i = 0; i < size; ++i) {
                 TreeNode* node = q.front();
                 q.pop();
-                // if its the last node at this level
-                // record it
                 if (i == size - 1) result.push_back(node->val);
-                // now add children to queue
                 if (node->left) q.push(node->left);
                 if (node->right) q.push(node->right);
             }
